d3511aa714198304435fe9b85446ab5f33b59a9e
Squirrels <chocro@gmail.com>
2018-11-09 14:34:30 -0800

    Working storage with redux-persist

---
+++ components/ExerciseDetailsView.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, TextInput, Button, Platform } from 'react-native';
+
+export class ExerciseDetailsView extends React.Component {
+  constructor(props) {
+    super(props);
+    // Check if we got an exercise from props
+    let exercise = props.existingExercise;
+    if(exercise !== undefined){
+      this.state = {
+        name: exercise.name,
+        updating: true
+      };
+    } else {
+      this.state = {
+        name: '',
+        updating:false
+      };
+    }
+    this.handlePress = this.handlePress.bind(this);
+  }
+
+  handlePress(){
+    // Callback
+    
+    this.props.onExerciseSave({workout: this.props.workout, exercise: this.props.existingExercise, new_exercise: {name: this.state.name}});
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Name:</Text>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Exercise1"
+          value={this.state.name}
+          onChangeText={(name) => this.setState({name})}
+          onSubmitEditing={() => this.handlePress() }
+          autoFocus={true}
+        />
+        <Button
+          onPress={() => this.handlePress() }
+          title={this.state.updating ? "Update" : "Create"}
+          color={Platform.OS === 'ios' ? '#fff' : null}
+        />
+      </View>
+    );
+  }
+}
